4fc3f4fd64bbfeffed71c8390c3a71aa7cfce03c
asdetmin <alexanderpurkhalo@gmail.com>
2016-05-10 10:27:25 -0400

    class example

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,12 @@
-import React from 'react'; // we need React because <input /> still is React.createElement
+import React, { Component } from 'react'; // we need React because <input /> still is React.createElement
+// { Component } is like const Component = React.component
 
-const SearchBar = () => {
-  return <input />
-};
+class SearchBar extends Component {
+    render() {
+        return <input />;
+    }
+}
+// '... extends React.Component ...'  allow to get all properties that React has
+// render is a function
 
 export default SearchBar; // To make valid a component in other files
