3bd92d19143156822f4e269834f5977ff9354878
asdetmin <alexanderpurkhalo@gmail.com>
2016-05-10 09:59:20 -0400

    youtube search-installing and getting a API key

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-
+// https://console.developers.google.com/apis
+const API_KEY = 'AIzaSyCk7TOyegJEKqMMrUjII3fNjDNTcja5sxY'
+// npm install --save youtube-api-search
+ReactDOM.render();
