4f268e6220668a9430419f5ab2d8459f7fb24694
asdetmin <alexanderpurkhalo@gmail.com>
2016-05-10 13:24:20 -0400

    sets a stete of videos & code refactoring

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,19 +1,13 @@
 import React, { Component } from 'react';
 
-// Class Base Component
 class SearchBar extends Component {
     constructor(props) {
         super(props);
 
-        // value can be anything, for example:
         this.state = { term: '' };
-        // this.state = { term: 'some text' };
-        // this.state = { term: true };
-        // this.state = { term: 666 };
     }
     render() {
         return (
-            // input now is 'control component'
             <div>
                 <input
                     onChange={event => this.setState({ term: event.target.value })}
--- src/index.js
+++ src/index.js
@@ -1,23 +1,34 @@
-// Document Configuration
-import React from 'react';
+// Basic Document Configuration
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import YTSearch from 'youtube-api-search';
-const API_KEY = 'AIzaSyCk7TOyegJEKqMMrUjII3fNjDNTcja5sxY';
 
 // Import of Components
 import SearchBar from './components/search_bar';
 
+// Additional Configurations
+import YTSearch from 'youtube-api-search'; // npm install --save youtube-api-search
+const API_KEY = 'AIzaSyCk7TOyegJEKqMMrUjII3fNjDNTcja5sxY'; // https://console.developers.google.com/apis
 
-// here we can see configurations and callback function
-YTSearch({ key: API_KEY, term: 'surfboards'}, function(data) {
-    console.log(data);
-});
-// Functional Base Component
-const App = () => {
-    return <div>
-        <SearchBar />
-    </div>
-};
-// https://console.developers.google.com/apis
-// npm install --save youtube-api-search
+
+
+class App extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = { videos: [] }; // an empty array as value
+
+        YTSearch({ key: API_KEY, term: 'surfboards'}, (videos) => {
+            // below our empty array is filling by objects from "videos" parameter. Example: videos: [ {...}, {...} ... ]
+            this.setState({ videos }); // same as: "videos: videos", this is just refactoring
+
+        });
+    }
+    render(){
+        return(
+            <div>
+                <SearchBar />
+            </div>
+        );
+    }
+}
 ReactDOM.render(<App />, document.querySelector('.container'));
