dc411d760772acc33c19072caa95a8dd347afa5b
asdetmin <alexanderpurkhalo@gmail.com>
2016-05-10 14:41:16 -0400

    iterating and showing video throw videos array with map() method

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import VideoListItem from './video_list_item';
 
-const VideoList = (props) => { // here we pass "props" as argument that has special role as inherited element
-    // const videos = props.videos; // this will be an array of videos
-    // videos is just variable from from other object
+const VideoList = (props) => {
+    // firstly we add array 'props.videos'
+    // then we iterate throw every element of array via 'map()' method ... Ruby this: each { ... }
+    // net we pass function into map method with 'video' parameter that plays role as each element in array ..
+    // something like that: props.array.map(function(item) { return item }); ... Ruby this: array.each { |item| item }
+    // so if we have videos array with three objects, example: var array = [1, 2, 3]
+    // after getting a array.map(function(item){ return item * 3 }) method we will get following stuff: [3, 6, 9]
+    const videoItems = props.videos.map((video) => {
+        // here we pass every 'video' object from 'videos' array to 'video' instance
+        return <VideoListItem video={video} />; 
+    });
+
     return (
         <ul className="col-md-4 list-group">
-            <VideoListItem />
-            { props.videos.length }
+            { videoItems }
         </ul>
     )
 };
---
+++ src/components/video_list_item.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const VideoListItem = () => {
+    return <li></li>
+};
+export default VideoListItem;
