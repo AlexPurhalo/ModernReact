0735ad5260a99d2a521b9867fb4e44c583c3c02c
asdetmin <alexanderpurkhalo@gmail.com>
2016-05-10 13:00:07 -0400

    Adding YTSearch function with configurations and callback function

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
+// Document Configuration
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+const API_KEY = 'AIzaSyCk7TOyegJEKqMMrUjII3fNjDNTcja5sxY';
 
+// Import of Components
 import SearchBar from './components/search_bar';
 
-// Functional Base Component 
-const MainBox = () => {
+
+// here we can see configurations and callback function
+YTSearch({ key: API_KEY, term: 'surfboards'}, function(data) {
+    console.log(data);
+});
+// Functional Base Component
+const App = () => {
     return <div>
         <SearchBar />
     </div>
 };
 // https://console.developers.google.com/apis
-const API_KEY = 'AIzaSyCk7TOyegJEKqMMrUjII3fNjDNTcja5sxY';
 // npm install --save youtube-api-search
-ReactDOM.render(<MainBox />, document.querySelector('.container'));
+ReactDOM.render(<App />, document.querySelector('.container'));
